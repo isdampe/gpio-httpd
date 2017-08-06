f515a71883bd70e83f48b88c49884eab17246701
Richard Denton <isdampe@gmail.com>
2017-08-06 12:08:48 +1000

    Progress on http_parse

--- src/http-parser.h
+++ src/http-parser.h
@@ -1,12 +1,12 @@
+#define HTTPD_MTHD_UNKNOWN                    0
+#define HTTPD_MTHD_GET                        1
+#define HTTPD_MTHD_POST                       2
+
 typedef struct request {
   unsigned char method;
   char *uri;
   unsigned int error;
 } request;
-
-#define HTTPD_MTHD_UNKNOWN 0
-#define HTTPD_MTHD_GET     1
-#define HTTPD_MTHD_POST     2
 
 array* httpd_str_split(char *delim, char *buffer);
 request httpd_parse_request(char *buffer);
